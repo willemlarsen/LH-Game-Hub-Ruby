6905bd6571e05cddbabceaf5b29c9d9c998ef277
Willem Larsen <willemlarsen@gmail.com>
2014-08-11 10:29:34 -0700

    first commit after rewrite

--- lib/two_person_lap.rb
+++ lib/two_person_lap.rb
@@ -1,18 +1,5 @@
-require 'question'
-
 class TwoPersonLap
-  def initialize(language)
-    @language = language
-    @topic = { theme: 'food and drink', prop: 'empty cup' }
-    @what = QuestionAndAnswer.new
-    @who = QuestionAndAnswer.new
+  def initialize
   end
-
-  attr_reader :language
-
-  attr_accessor :topic
-
-  attr_accessor :what
-
-  attr_accessor :who
 end
+
--- spec/two_person_lap_spec.rb
+++ spec/two_person_lap_spec.rb
@@ -1,49 +1,9 @@
 require 'rspec'
 require 'two_person_lap'
-require 'question'
 
-describe TwoPersonLap do
-  it 'creates a new instance of itself' do
-    two_person_lap_test = TwoPersonLap.new('Hungarian')
-    expect(two_person_lap_test).to be_an_instance_of TwoPersonLap
-  end
-
-  it 'tells its chosen language' do
-    two_person_lap_test = TwoPersonLap.new('Hungarian')
-    expect(two_person_lap_test.language).to eq('Hungarian')
-  end
-
-  it 'has a default conversation topic' do
-    two_person_lap_test = TwoPersonLap.new('Hungarian')
-    expect(two_person_lap_test.topic[:theme]).to eq('food and drink')
-  end
-
-  it 'has a default conversation prop' do
-    two_person_lap_test = TwoPersonLap.new('Hungarian')
-    expect(two_person_lap_test.topic[:prop]).to eq('empty cup')
-  end
-
-  it 'can set the question for the What? square' do
-    two_person_lap_test = TwoPersonLap.new('Hungarian')
-    two_person_lap_test.what.question = 'Mi ez?'
-    expect(two_person_lap_test.what.question).to eq('Mi ez?')
-  end
-
-  it 'can set the answer for the What? square' do
-    two_person_lap_test = TwoPersonLap.new('Hungarian')
-    two_person_lap_test.what.answer = 'Ez egy csésze?'
-    expect(two_person_lap_test.what.answer).to eq('Ez egy csésze?')
-  end
-
-  it 'can set the answer for the Who? square' do
-    two_person_lap_test = TwoPersonLap.new('Hungarian')
-    two_person_lap_test.who.question = 'Ez a csészém?'
-    expect(two_person_lap_test.who.question).to eq('Ez a csészém?')
-  end
-
-  it 'can set the answer for the What? square' do
-    two_person_lap_test = TwoPersonLap.new('Hungarian')
-    two_person_lap_test.what.answer = 'Igen, az a czészéd.'
-    expect(two_person_lap_test.what.answer).to eq('Igen, az a czészéd.')
+describe 'TwoPersonLap' do
+  it 'creates an instance of itself' do
+    test_lap = TwoPersonLap.new
+    expect(test_lap).to be_an_instance_of TwoPersonLap
   end
 end
