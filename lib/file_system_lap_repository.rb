53e0f81b79d93dc5590d48e70c98b833ce62f5d5
Willem Larsen <willemlarsen@gmail.com>
2014-09-03 20:40:29 -0700

    private method is private -SLG

--- lib/file_system_lap_repository.rb
+++ lib/file_system_lap_repository.rb
@@ -1,6 +1,7 @@
 require './lib/lap'
 
 class FileSystemLapRepository
+  attr_reader :lap, :directory
   def initialize(directory)
     @lap = Lap.new
     @directory = directory
@@ -8,10 +9,10 @@ class FileSystemLapRepository
     record_exchange('who')
   end
 
+  private
+
   def record_exchange(question_word)
     @lap.record_question(question_word, File.read(directory + "/#{question_word}/question.txt"))
     @lap.record_answer(question_word, File.read(directory + "/#{question_word}/answer.txt"))
   end
-
-  attr_reader :lap, :directory
 end
