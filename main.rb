f841e5acfc7d81cd27013a321cf5995cfe365662
Willem Larsen <willemlarsen@gmail.com>
2014-09-07 14:23:35 -0700

    cleanup UI and grab text-to-speech from google

--- main.rb
+++ main.rb
@@ -3,34 +3,53 @@ require './lib/game.rb'
 require './lib/lap.rb'
 require './lib/lap_repository.rb'
 
-puts "Hello! Welcome to the Language Hunt Game Hub."
+def introduce_game_hub
+  puts "Hello! Welcome to the Language Hunt Game Hub."
 
-current_game_repo = LapRepository.new('test_data/lap_one')
+  @current_game_repo = LapRepository.new('test_data/lap_one')
 
-Player.new(current_game_repo).show_script
+  Player.new(@current_game_repo).show_script
 
-puts ""
-puts "Which Question square are you on (What, Who, Where, etc.)?"
-
-square = gets.chomp.downcase
-
-puts ""
-puts "#{square.upcase}:"
-puts "Question: #{current_game_repo.lap.retrieve_question(square)}"
-puts "Answer: #{current_game_repo.lap.retrieve_answer(square)}"
-puts ""
-puts "Press 'q' to play question audio, or 'a' to play answer, and hit enter."
-
-audio_choice = gets.chomp.downcase
-
-if audio_choice == 'q'
-  text_to_speech = current_game_repo.lap.question_squares[square][:question]
-  puts text_to_speech
-else
-  text_to_speech = current_game_repo.lap.question_squares[square][:question]
-  puts text_to_speech
+  puts ""
+  choose_question_square
 end
 
+def choose_question_square
+  puts "Enter which Question square you are on (What, Who, Where, etc.), or any key to quit."
 
+  @square = gets.chomp.downcase
 
+  if @current_game_repo.lap.question_squares.key?(@square)
+      puts ""
+      puts "#{@square.upcase}:"
+      puts "Question: #{@current_game_repo.lap.retrieve_question(@square)}"
+      puts "Answer: #{@current_game_repo.lap.retrieve_answer(@square)}"
+      puts ""
+      audio_choice_interface
+    else
+      exit
+    end
+end
+
+def audio_choice_interface
+  puts "Press 'q' to play question audio, or 'a' to play answer, or any key to change squares, and hit enter."
+
+  @audio_choice = gets.chomp.downcase
+
+  if @audio_choice == 'q'
+    text_to_speech = @current_game_repo.lap.question_squares[@square][:question]
+    formatted_text_to_speech = text_to_speech.gsub(" ", "+")
+    `wget -q -U Mozilla -O output.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=hu&q=#{formatted_text_to_speech}" ; afplay output.mp3 ; rm output.mp3`
+
+  elsif @audio_choice == 'a'
+    text_to_speech = @current_game_repo.lap.question_squares[@square][:answer]
+    formatted_text_to_speech = text_to_speech.gsub(" ", "+")
+    `wget -q -U Mozilla -O output.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=hu&q=#{formatted_text_to_speech}" ; afplay output.mp3 ; rm output.mp3`
+  else
+    choose_question_square
+  end
+  audio_choice_interface
+end
+
+introduce_game_hub
 
